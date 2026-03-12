6727296de30e6f284ab86095634ff6813644481d
Veerapaneni BalaChaitanya <veerapanenibala8@gmail.com>
2026-03-12 10:27:11 +0530

    Fix indentation in prime number check

--- prime number.py
+++ prime number.py
@@ -2,9 +2,10 @@ n=int(input('Enter a number'))
 if n>1:
     for i in range(2,n//2+1):
         if n%i==0:
-            print('Number is not a prime number')
+            print('Number is not a prime')
             break
     else:
-        print('Number is prime number')
+        print('Number is prime')
 else:
     print('Number is not a prime')
+
